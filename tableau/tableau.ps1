9d49b34e8d352723a2c6136a91d8b01ad5948d41
Anton Seitz <aseitz@puckit.de>
2020-05-12 16:08:32 +0200

    FIX Tableau

--- tableau/tableau.ps1
+++ tableau/tableau.ps1
@@ -3,19 +3,31 @@
 ##############################################################
 
 
+# READ CONFIG 
+
+. $PSScriptRoot\tableau_config.ps1
+
+
+if( -not (test-path $tableau_dump_folder)){
+write "Folder " +$tableau_dump_folder  + " not here! Creating it.."
+md $tableau_dump_folder
+
+}
+
+
 $datum=get-date;
 
 
 
 # Logfile mit gleichem Namen wie Script in gleichem Ordner ablegen:
-$executingScriptDirectory = Split-Path -Path $MyInvocation.MyCommand.Definition -Parent
-$logfile = $executingScriptDirectory + $MyInvocation.MyCommand.Name + ".log.txt"
-write-output "###### >  START REPORT  BACKUP DAILY " >> $logfile 
-Write-Output "$($datum.dayofweek) $($datum.hour)" *>> $logfile 
+#$executingScriptDirectory = Split-Path -Path $MyInvocation.MyCommand.Definition -Parent
+#$logfile = $executingScriptDirectory + "\logs/" + $MyInvocation.MyCommand.Name + ".log.txt"
+write-output "###### >  START REPORT  BACKUP DAILY " #>> $logfile 
+Write-Output "$($datum.dayofweek) $($datum.hour)" #*>> $logfile 
 
  
 
- Write-Output "Tableau Backup erstellen..." *>> $logfile 
+ Write-Output "Tableau Backup erstellen..." #*>> $logfile 
 # Tableau Backup erstellen
 #$tabpath = "C:\Program Files\Tableau\Tableau Server\10.5\bin\tabadmin.exe"
 
@@ -27,27 +39,29 @@ $tabexe = $tabpath + "\tsm.cmd"
 
 
 
-& $tabpath backup c:\backup\tableau\tabbackup.$($datum.dayofweek)_$($datum.hour)  *>> $logfile
-if ( $LASTEXITCODE -gt 0 ) {
-write $LASTEXITCODE
-exit}
-Write-Output "TSM Login " *>> $logfile 
+#& $tabexe backup c:\backup\tableau\tabbackup.$($datum.dayofweek)_$($datum.hour)  #*>> $logfile
+#if ( $LASTEXITCODE -gt 0 ) {
+#write $LASTEXITCODE
+#exit}
 
-& $tabexe login --username administrator --password "passwd," *>> $logfile
+Write-Output "TSM Login " #*>> $logfile 
 
-Write-Output "TSM Export Settings " *>> $logfile 
-& $tabexe settings export -f c:\backup\tableau\tabbackup.settings.$($datum.dayofweek)_$($datum.hour).json   *>> $logfile
+& $tabexe login --username $tableau_user --password $tableau_pwd #*>> $logfile
+
+Write-Output "TSM Export Settings " #*>> $logfile 
+& $tabexe settings export -f $tableau_bckp_local_target\tabbackup.settings.$($datum.dayofweek)_$($datum.hour).json  # *>> $logfile
  
-Write-Output "TSM Backup Data " *>> $logfile 
-& $tabexe maintenance backup -f tabbackup.data.tsbak --override-disk-space-check *>> $logfile
+Write-Output "TSM Backup Data " #*>> $logfile 
+$tsbak = "tabbackup.data." + $($datum.dayofweek) + "_" + $($datum.hour) + ".tsbak"
+& $tabexe maintenance backup -f $tsbak --override-disk-space-check #*>> $logfile
 
 # nach e: verschieben
-Write-Output "...nach E: verschieben... " *>> $logfile 
-$tsbackup = $env:TABLEAU_SERVER_DATA_DIR + "\data\tabsvc\files\backups\tabbackup.data.tsbak"
-move-item $tsbackup c:\backup\tableau\tabbackup.data.$($datum.dayofweek)_$($datum.hour).tsbak -force
-Write-Output "...verschoben" *>> $logfile 
+Write-Output "...nach E: verschieben... " #*>> $logfile 
+$tsbackup = $env:TABLEAU_SERVER_DATA_DIR + "\data\tabsvc\files\backups\" + $tsbak
+move-item $tsbackup $tableau_bckp_local_target\$tsbak -force
+Write-Output "...verschoben" #*>> $logfile 
 
-Write-Output "Tableau Backup beendet" *>> $logfile 
+Write-Output "Tableau Backup beendet" #*>> $logfile 
  
  
 
---
+++ tableau/tableau_config_default.ps1
@@ -0,0 +1,3 @@
+$tableau_bckp_local_target$tableau_dump_folder="c:\ms_backup\tableau\local_backup"
+$tableau_user="administrator"
+$tableau_pwd="password"
