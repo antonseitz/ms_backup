f491558e8150b4309a84589dc86f006b0a277c05
DMB aseitz <aseitz@museumsbund.de>
2020-05-18 13:53:25 +0200

    MSSQL

--- mssql/mssql.ps1
+++ mssql/mssql.ps1
@@ -1,9 +1,16 @@
-
+"MSSQL: START"
 import-module sqlserver
 
 
 
 . $PSScriptRoot\mssql_config.ps1
+
+if( -not (test-path $mssql_dump_folder)){
+write "Folder " +$mssql_dump_folder  + " not here! Creating it.."
+md $mssql_dump_folder
+
+}
+
 
 
 if (-not $Whole_instances ){
@@ -16,7 +23,7 @@ if ($dbs) {
 $dbs.gettype()
 foreach ($db in $dbs){
  $db[0] +  $db[1]
- Backup-SqlDatabase -serverinstance $db[0] -database $db[1]  -backupcontainer $backuppath -verbose
+ Backup-SqlDatabase -serverinstance $db[0] -database $db[1]  -backupcontainer $mssql_dump_folder -verbose
  
  
 }
@@ -25,7 +32,7 @@ foreach ($db in $dbs){
 }
 if ($whole_instances) {
 
-write "\n Backing up whole instances \n"
+write " Backing up whole instances "
 
 foreach ($instance in $whole_instances){
 
@@ -34,21 +41,23 @@ $dbs=Get-ChildItem SQLSERVER:\SQL\$instance\Databases -force   | Where { $_.Name
 $full_dbs=Get-ChildItem SQLSERVER:\SQL\$instance\Databases -force |  where {$_.Recoverymodel -eq "FULL"}
 
 
-# Database Backup:
-$dbs | Backup-SqlDatabase -backupaction database -backupcontainer $backuppath -verbose
+"MSSQL:  Database Backup:"
+$dbs | Backup-SqlDatabase -backupaction database -backupcontainer $mssql_dump_folder -verbose
 
 
 
 }
 
-# T-LOG Backup:
-$full_dbs | Backup-SqlDatabase -backupaction log -backupcontainer $backuppath -verbose
+"MSSQL: Make T-LOG Backup"
+$full_dbs | Backup-SqlDatabase -backupaction log -backupcontainer $mssql_dump_folder -verbose
 
 
 
 
 
 }
+
+"MSSQL: DONE!"
 
 
 
