febf7e5fe5a84b1f8b0d7daf2e84456db4f570ea
DMB aseitz <aseitz@museumsbund.de>
2020-05-18 18:08:46 +0200

    MSSQL

--- mssql/mssql.ps1
+++ mssql/mssql.ps1
@@ -2,7 +2,7 @@
 import-module sqlserver
 
  if($script:debug) {$verbose = " -verbose "}
- else {$verbose = $null}
+ else {$verbose = ""}
 
 if( -not (test-path $PSScriptRoot\mssql_config.ps1)){
 write "MSSQL: Config  " +$PSScriptRoot\mssql_config.ps1  + " not here!"
@@ -30,7 +30,7 @@ if ($dbs) {
 $dbs.gettype()
 foreach ($db in $dbs){
  $db[0] +  $db[1]
- Backup-SqlDatabase -serverinstance $db[0] -database $db[1]  -backupcontainer $mssql_dump_folder -verbose
+ Backup-SqlDatabase -serverinstance $db[0] -database $db[1]  -backupcontainer $mssql_dump_folder $verbose
  
  
 }
@@ -50,13 +50,25 @@ $full_dbs=Get-ChildItem SQLSERVER:\SQL\$instance\Databases -force |  where {$_.R
 
 "MSSQL:  Database Backup:"
 if( -not $script:dryrun){
-$dbs | Backup-SqlDatabase -backupaction database -backupcontainer $mssql_dump_folder\last $verbose
+if($script:debug){
+$dbs | Backup-SqlDatabase -backupaction database -backupcontainer $mssql_dump_folder\last -verbose
+
+else{
+
+$dbs | Backup-SqlDatabase -backupaction database -backupcontainer $mssql_dump_folder\last 
+}
+
 } else { "DRYRUN: skipped!"}
 
 "MSSQL: Make T-LOG Backup"
 if( -not $script:dryrun){
-$full_dbs | Backup-SqlDatabase -backupaction log -backupcontainer $mssql_dump_folder\last $verbose
-} else { "DRYRUN: skipped!"}
+
+if($script:debug){
+$full_dbs | Backup-SqlDatabase -backupaction log -backupcontainer $mssql_dump_folder\last -verbose
+}else{
+$full_dbs | Backup-SqlDatabase -backupaction log -backupcontainer $mssql_dump_folder\last 
+ }
+ else { "DRYRUN: skipped!"}
 
 }
 "MSSQL Backup of " +  $instance + "  done"
