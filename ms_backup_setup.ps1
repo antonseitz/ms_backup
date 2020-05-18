02f7bbd9c47abf676364a82308fef51745279a06
Anton Seitz <aseitz@puckit.de>
2020-05-18 13:07:13 +0200

    SETUP FIX

--- ms_backup_setup.ps1
+++ ms_backup_setup.ps1
@@ -1,27 +1,40 @@
 param(
-[string]$full_diff 
-#[switch]$debug ,
+[string]$full_diff ,
+[switch]$debug 
 #[switch]$dryrun
 )
-
+# DEBUG ?
 Set-PSDebug -off
+
+if($debug) {Set-PSDebug -Trace 1}
+else {Set-PSDebug -Off}
+
+
+
+# ARE YOU ADMIN ?
+
+$currentPrincipal = New-Object Security.Principal.WindowsPrincipal([Security.Principal.WindowsIdentity]::GetCurrent())
+if ( -not $currentPrincipal.IsInRole([Security.Principal.WindowsBuiltInRole]::Administrator) ){
+write "YOU HAVE NO ADMIN RIGHTS: EXITING!"
+exit 1
+}
+
+# USAGE
 
 if (! $full_diff -or (($full_diff -ne "full") -and ($full_diff -ne "diff"))){ 
 write-host("Usage: "  +  $MyInvocation.MyCommand.Name + " -full_diff full|diff ") #[-debug] [-dryrun] ")
-#write "-debug = a lot of output"
+write "-debug = a lot of output"
 #write "-dryrun = without producing dumps and backup on target"
 
 exit
 }
 
 
-# ist feature installed ?
+# Install Feature "Windows Server Backup" , if not installed 
 if ( -not (Get-WindowsFeature | where { $_.Name -eq "Windows-Server-Backup"  -and $_.installstate -eq "installed" })){
-
 Add-WindowsFeature Windows-Server-Backup
 
 }
-
 
 
 
@@ -36,8 +49,12 @@ $pass=read-host "Password" -AsSecureString # do not show password
 $pass=[Runtime.InteropServices.Marshal]::PtrToStringAuto(
     [Runtime.InteropServices.Marshal]::SecureStringToBSTR($pass))
 
+$arg= '-noninteractive -noLogo -noprofile -command "& {c:\ms_backup\ms_backup.ps1 ' + $full_diff  + '; return $LASTEXITCODE  }"  2>&1 > c:\ms_backup\logs\ms_backup.' + $full_diff + '.log'
+$arg
 
-$A = New-ScheduledTaskAction -Execute "powershell.exe" -Argument "-noninteractive -noLogo -noprofile -command ""& {c:\ms_backup\ms_backup.ps1 $full_diff ; return $LASTEXITCODE  }""  2>&1 > c:\ms_backup\logs\ms_backup.$full_diff.log"
+
+
+$A = New-ScheduledTaskAction -Execute "powershell.exe" -Argument $arg
 $T = New-ScheduledTaskTrigger -Daily -At 9pm
 #$P = New-ScheduledTaskPrincipal "domain|computer\backup" -logontype Password
 
