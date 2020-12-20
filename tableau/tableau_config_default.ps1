d4422a122f59e8688ec162d9eea753c3bc8a6247
Anton Seitz <aseitz@puckit.de>
2020-12-20 21:31:04 +0100

    tableau default config

--- tableau/tableau_config_default.ps1
+++ tableau/tableau_config_default.ps1
@@ -1,3 +1,9 @@
-$tableau_bckp_local_target$tableau_dump_folder="c:\ms_backup\tableau\local_backup"
+$tableau_dump_folder="c:\ms_backup\tableau\dumps"
+$tableau_dump_folder_last="c:\ms_backup\tableau\dumps\last"
 $tableau_user="administrator"
-$tableau_pwd="password"
+$tableau_pwd="password,"
+
+
+$diff_files_locations += new-wbfilespec $tableau_dump_folder
+
+$rotate_dirs+=$tableau_dump_folder
