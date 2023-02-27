d9e1551d5c2e1b16e5359c809f4da4a8c4104c24
antonseitz <aseitz@puckit.de>
2023-02-27 18:48:29 +0100

    vitrual box backup

---
+++ virtualbox/virtualbox.ps1
@@ -0,0 +1,14 @@
+##############################################################
+# Backup Skript f. VirutalBox #
+##############################################################
+
+
+$diff_files_locations += new-wbfilespec "E:\vms\"
+
+
+
+C:\Program Files\Oracle\VirtualBox\VBoxManage.exe 
+
+ 
+  
+  
